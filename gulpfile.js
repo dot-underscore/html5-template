63a250ef9d93e7adc1b8240ea0a3e8ad41a69536
Joost van Cadsand <joost@damnright.nl>
2015-03-19 16:45:45 +0100

    icon font creator, error handler

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 //config
-var devUrl = "url.dev";
+var devUrl = "template.dev";
+var fontName = "fontname";
 var jsFiles = [
-            './js/jquery-1.11.2.js', 
-            './js/main.js' 
+            './assets/js/jquery-1.11.2.js', 
+            './assets/js/main.js' 
         ]
 var prefixString= ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
 
@@ -11,6 +12,7 @@ var prefixString= ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', '
 var gulp = require('gulp'); 
 
 // Include Our Plugins
+var plumber     = require('gulp-plumber');
 var jshint      = require('gulp-jshint');
 var sass        = require('gulp-sass');
 var sourcemaps  = require('gulp-sourcemaps');
@@ -19,39 +21,57 @@ var prefix      = require('gulp-autoprefixer');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
 var rename      = require('gulp-rename');
+var fontcustom  = require('gulp-fontcustom');
+var notify      = require("gulp-notify");
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/main.js')
+    return gulp.src('assets/js/main.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('assets/scss/*.scss')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(prefix(prefixString))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
-        .pipe(reload({stream: true}));
+        .pipe(gulp.dest('assets/css'))
+        .pipe(reload({stream: true}))
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src(jsFiles)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(concat('main.min.js'))
-        .pipe(gulp.dest('js/min'))
+        .pipe(gulp.dest('assets/js/min'))
         .pipe(reload({stream: true}));
+});
+
+// Create website font
+gulp.task('font', function() {
+    return gulp.src('assets/img/icons/*.svg')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(fontcustom({
+          font_name: fontName,
+          templates: "scss"
+        }))
+        .pipe(gulp.dest("assets/fonts/customfont"))
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('assets/js/*.js', ['scripts']);
+    gulp.watch('assets/scss/*.scss', ['sass']);
+    gulp.watch('assets/img/icons/*.svg', ['font']);
+    gulp.watch('index.*', reload);
+
 });
 
 //Start browser sync server
@@ -70,12 +90,14 @@ gulp.task('browser-sync', function() {
 //Minify, uglify production ready
 gulp.task('scriptsProduction', function(){
     return gulp.src(jsFiles)
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))    
         .pipe(concat('main.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/min'));
+        .pipe(gulp.dest('assets/js/min'));
 })
 gulp.task('sassProduction', function(){
-    return gulp.src('scss/*.scss')
+    return gulp.src('assets/scss/*.scss')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
         .pipe(sass())
         .pipe(prefix(prefixString))
         .pipe(minifycss())
@@ -83,5 +105,5 @@ gulp.task('sassProduction', function(){
 })
 
 // Default Task
-gulp.task('default', ['sass', 'scripts', 'browser-sync', 'watch']);
-gulp.task('production', ['sassProduction','scriptsProduction', 'lint']);
+gulp.task('default', ['sass', 'scripts', 'font', 'browser-sync', 'watch']);
+gulp.task('production', ['sassProduction','scriptsProduction', 'font', 'lint']);
